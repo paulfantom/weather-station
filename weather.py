dbfa51237593996cb6134fd5a8fcc6e606a4395d
paulfantom <paulfantom@gmail.com>
2013-12-20 17:26:22 +0100

    add "pressure" option in current weather section

--- weather.py
+++ weather.py
@@ -3,10 +3,13 @@
 import urllib2
 import json
 
+#global API_KEY
+API_KEY="fecfc874ac6ad136"
+
 class Weather:
-  def __init__(self,location="autoip",options="conditions/forecast"):
+  def __init__(self,location="autoip",options="conditions/forecast",apikey=API_KEY):
     wundergroundResponse = urllib2.urlopen(
-      "http://api.wunderground.com/api/fecfc874ac6ad136/" + options + "/q/" + location + ".json").read()
+      "http://api.wunderground.com/api/" + apikey + "/" + options + "/q/" + location + ".json").read()
     self.data = json.loads(wundergroundResponse)
 
   def conditions(self):
@@ -14,8 +17,9 @@ class Weather:
       'temp'     : str(    self.data["current_observation"]["temp_c"]),
       'humidity' : str(    self.data["current_observation"]["relative_humidity"]),
       'feeltemp' : str(    self.data["current_observation"]["feelslike_c"]),
-      'wind'     : str(int(self.data["current_observation"]["wind_kph"]*4/9)),
-      'sky'      : str(    self.data["current_observation"]["weather"])
+      'sky'      : str(    self.data["current_observation"]["weather"]),
+      'pressure' : str(    self.data["current_observation"]["pressure_mb"]),
+      'wind'     : str(int(self.data["current_observation"]["wind_kph"]*4/9))
     }
 
   def forecast(self):
@@ -24,22 +28,22 @@ class Weather:
         'temp_high' : str(    self.data["forecast"]["simpleforecast"]["forecastday"][0]["high"]["celsius"]),
         'temp_low'  : str(    self.data["forecast"]["simpleforecast"]["forecastday"][0]["low"]["celsius"]),
         'humidity'  : str(    self.data["forecast"]["simpleforecast"]["forecastday"][0]["avehumidity"]),
-        'wind'      : str(int(self.data["forecast"]["simpleforecast"]["forecastday"][0]["avewind"]["mph"]*4/9)),
-        'sky'       : str(    self.data["forecast"]["simpleforecast"]["forecastday"][0]["conditions"])
+        'sky'       : str(    self.data["forecast"]["simpleforecast"]["forecastday"][0]["conditions"]),
+        'wind'      : str(int(self.data["forecast"]["simpleforecast"]["forecastday"][0]["avewind"]["mph"]*4/9))
         },
       "tomorrow": {
        'temp_high'  : str(    self.data["forecast"]["simpleforecast"]["forecastday"][1]["high"]["celsius"]),
        'temp_low'   : str(    self.data["forecast"]["simpleforecast"]["forecastday"][1]["low"]["celsius"]),
        'humidity'   : str(    self.data["forecast"]["simpleforecast"]["forecastday"][1]["avehumidity"]),
-       'wind'       : str(int(self.data["forecast"]["simpleforecast"]["forecastday"][1]["avewind"]["mph"]*4/9)),
-       'sky'        : str(    self.data["forecast"]["simpleforecast"]["forecastday"][1]["conditions"])
+       'sky'        : str(    self.data["forecast"]["simpleforecast"]["forecastday"][1]["conditions"]),
+       'wind'       : str(int(self.data["forecast"]["simpleforecast"]["forecastday"][1]["avewind"]["mph"]*4/9))
        },
       "dayafter": {
        'temp_high'  : str(    self.data["forecast"]["simpleforecast"]["forecastday"][2]["high"]["celsius"]),
        'temp_low'   : str(    self.data["forecast"]["simpleforecast"]["forecastday"][2]["low"]["celsius"]),
        'humidity'   : str(    self.data["forecast"]["simpleforecast"]["forecastday"][2]["avehumidity"]),
-       'wind'       : str(int(self.data["forecast"]["simpleforecast"]["forecastday"][2]["avewind"]["mph"]*4/9)),
-       'sky'        : str(    self.data["forecast"]["simpleforecast"]["forecastday"][2]["conditions"])
+       'sky'        : str(    self.data["forecast"]["simpleforecast"]["forecastday"][2]["conditions"]),
+       'wind'       : str(int(self.data["forecast"]["simpleforecast"]["forecastday"][2]["avewind"]["mph"]*4/9))
        }
 
     }
@@ -48,5 +52,6 @@ class Weather:
 if __name__ == '__main__':
   from pprint import pprint
   w = Weather("PL/Krakow")
+  print w.conditions()['sky']
   pprint(w.conditions())
   pprint(w.forecast())

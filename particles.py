82b8eecc26f06b642028181ec1ab4c41c4a26168
paulfantom <paulfantom@gmail.com>
2014-01-09 09:20:47 +0100

    air pollution

---
+++ particles.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+# graph from Krasinskiego @ 09 Jan 2014  
+# http://monitoring.krakow.pios.gov.pl/iseo/img/2014-01-09_005.png
+# all:
+# http://monitoring.krakow.pios.gov.pl/iseo/aktualne_stacja.php?stacja=005
+# 005 - Krasinskiego
+# 006 - Huta
+# 015 - Kurdwanów
+
+from HTMLParser import HTMLParser
+
+#handle_starttag("<td>Py³ zawieszony (PM<sub>10</sub>)")
+#handle_endtag("</tr>")
+
+if __name__ == '__main__':
+  pass

1a937470fc6fc77d342582ea1fc827a39f37114d
paulfantom <paulfantom@gmail.com>
2014-03-30 14:03:44 +0200

    maximize font size in image block object

--- daemon/imagine.py
+++ daemon/imagine.py
@@ -5,37 +5,69 @@ class Block():
   def __init__(self,size):
     self.size = size
     self.block = Image.new("L",size)
+    self.area  = ImageDraw.Draw(self.block)
 
-  def setFont(fontPath="/usr/share/fonts/dejavu/DejaVuSans.ttf",size=self.size[1])
-    self.font = ImageFont.truetype(fontPath,size)
-    return self
+  # private:
+  def __position(self,
+                 horizontal="center",
+                 vertical="center",
+                 rectangleSize=(0,0)):
+    if   horizontal == "center":
+      x = ( int(self.block.size[0]) - rectangleSize[0] ) / 2
+    elif horizontal == "right":
+      x =   int(self.block.size[0]) - rectangleSize[0]
+    else:         # == "left"
+      x = 0
 
-  def text(self,value,anchor="center"):
-    write = ImageDraw.Draw(self.block)
-    textSize = write.textsize(value,self.font)
-    if anchor == "center":
-      x = ( int(self.size[0]) - textSize[0] ) / 2
-      y = ( int(self.size[1]) - textSize[1] ) / 2
-    elif anchor == "up":
-      x = ( int(self.size[0]) - textSize[0] ) / 2
-      y =   int(self.size[1])
-#TODO add more anchors
-    elif type(anchor) == tuple:
-      x = anchor[0]
-      y = anchor[1]
+    if   vertical == "center":
+      y = ( int(self.block.size[1]) - rectangleSize[1] ) / 2
+    elif vertical == "down":
+      y =   int(self.block.size[1]) - rectangleSize[1]
+    else:       # == "up"
+      y = 0
 
+    return (int(x),int(y))
 
-    xy=(int(x),int(y))
-    write.text(xy,value,fill="white",font=self.font)
-    return self
+  def __maxFont(self,text,path):
+    size = 12
+    font = ImageFont.truetype(path,size)
+    while font.getsize(text)[0] < self.block.size[0] - 1:
+      size += 2
+      font = ImageFont.truetype(path,size)
+    while font.getsize(text)[1] > self.block.size[1]:
+      size -= 2
+      font = ImageFont.truetype(path,size)
+    return font
+
+  # public:
+  def text(self,
+           value,
+           font="/usr/share/fonts/dejavu/DejaVuSans.ttf",
+           horizontal="center",
+           vertical="center",
+           fontSize="max"):
+
+    if fontSize == "max":
+      font = self.__maxFont(value,font)
+    else:
+      font = ImageFont.truetype(font,fontSize)
+
+    textSize = self.area.textsize(value,font)
+
+    xy = self.__position(horizontal,vertical,textSize)
+
+    self.area.text(xy,value,fill="white",font=font)
 
   def join(self,another,orientation="right"):
     return self
 
 
-
+  def show(self):
+    self.block.show()
 
 
 if __name__ == '__main__':
-  
+  rect = Block((400,100))
+  rect.text("hellodupa")
+  rect.show()
 

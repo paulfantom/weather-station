18ce8dc0dae4cf73ea374c63d74a651942eefa23
paulfantom <paulfantom@gmail.com>
2014-03-31 20:44:20 +0200

    imagine.text justification added

--- interfaces/kindle/imagine.py
+++ interfaces/kindle/imagine.py
@@ -12,53 +12,34 @@ class Block():
       self.block = Image.open(path)
     else:
       self.block = Image.new("L",size,background)
-    self.area  = ImageDraw.Draw(self.block)
+#    self.area  = ImageDraw.Draw(self.block)
 
-  def __maxFont(self,text,path,lines=1):
-    fontSize = 0
-    while True:
-      fontSize += 2
+  def __maxFont(self,text,path,fontSize,lines=1):
+    if not fontSize:
+      fontSize = 4
+
+    font = ImageFont.truetype(path,fontSize)
+    while font.getsize(text)[1] < (self.block.size[1] / lines):
       font = ImageFont.truetype(path,fontSize)
-      if font.getsize(text)[0] > self.block.size[0] :
-        font = ImageFont.truetype(path,fontSize-2)
-        break
+      fontSize += 2
 
-    while font.getsize(text)[1] > self.block.size[1] / lines:
+    font = ImageFont.truetype(path,fontSize-6)
+
+    while font.getsize(text)[0] > self.block.size[0]:
       fontSize -= 2
       font = ImageFont.truetype(path,fontSize)
 
-    return font
+    return { font.getsize(text) : font }
 
-  def __position(self,
-                 horizontal="center",
-                 vertical="center",
-                 rectangleSize=(0,0),
-                 offset=(0,0)):
-    if   horizontal == "center":
-      x = ( self.block.size[0] - rectangleSize[0] ) / 2
-    elif horizontal == "right":
-      x =   self.block.size[0] - rectangleSize[0]
-    else:         # == "left"
-      x = 0
-
-    if   vertical == "center":
-      y = ( self.block.size[1] - rectangleSize[1] ) / 2
-    elif vertical == "down":
-      y =   self.block.size[1] - rectangleSize[1]
-    else:       # == "up"
-      y = 0
-
-    y -= int(rectangleSize[1]/4) #FIXME dafuq is this???
-    return (int(x+offset[0]),int(y+offset[1]))
-
-  def __margin(self,another,axis='x',justify="center"):
+  def __margin(self,first,second,axis='x',justify="center"):
     if axis == 'x':
       axis = 0
     else:
       axis = 1
-    if self.block.size[axis] < another.block.size[axis]:
-      a = max(self.block.size[axis],another.block.size[axis])
-      b = min(self.block.size[axis],another.block.size[axis])
+
+    if first[axis] < second[axis]:
+      a = max(first[axis],second[axis])
+      b = min(first[axis],second[axis])
       if justify == "right" or justify == "down":
         return a - b
       if justify == "left" or justify == "up":
@@ -67,7 +48,6 @@ class Block():
         return (a - b) / 2
     else:
       return 0
-    
 
   ### -------------------- ###
   def text(self,
@@ -82,73 +62,72 @@ class Block():
     else:
       colour = "white"
 
-    value = value.splitlines()
+    value      = value.splitlines()
+    linesCount = len(value)
 
     if fontSize == "max":
-      font = self.__maxFont(max(value,key=len),fontPath,len(value))
-    else:
-      font = ImageFont.truetype(fontPath,fontSize)
+      fontSize = None
 
-#    x,y = 0,0
-#    for line in value:
-#      textSize = self.area.textsize(line,font)
-#      x  = max(x,textSize[0])
-#      y  = max(y,textSize[1])
-#      print x,y
-#    textSize = (x,y)
-    textSize = self.area.textsize(max(value,key=len),font)
+    fonts = {}
+    for i in value:
+      font = self.__maxFont(i,fontPath,fontSize,linesCount)
+      fonts = dict(fonts.items() + font.items())
 
-    multipliers = range(len(value))
-    if vertical == "down":
-      multipliers.reverse()
-      multipliers = [x * -1 for x in multipliers]
-    elif vertical == "center":     # == "center"
-      if len(multipliers)%2 == 1: # odd number
-        for i in range(len(value)):
-          multipliers[i] = i - int(len(value)/2)
-      else: # even number
-        for i in range(len(value)):
-          if i%2 == 0:
-            multipliers[i] = -0.5*i - 0.5
-          else:
-            multipliers[i] = 0.5*i
+    minimal = min(fonts.keys(), key = lambda t:t[1])
+    font = fonts.get(minimal)
+
+    area  = ImageDraw.Draw(self.block)
+    textSize = []
+    h = 0
+    for line in value:
+      textSize.append(area.textsize(line,font))
+      h = max(h,textSize[-1][1])
+    for i in range(linesCount):
+      textSize[i] = (textSize[i][0],h)
+
+    fullH = h * (linesCount + 1 )
+
+    y = self.__margin((textSize[0][0],fullH),self.block.size,'y',vertical)
 
     for idx,line in enumerate(value):
-      offset = multipliers[idx]*textSize[1]
-      xy = self.__position(horizontal,vertical,textSize,(0,offset))
-      self.area.text(xy,line,fill=colour,font=font)
+      x = self.__margin(textSize[idx],self.block.size,'x',horizontal)
+      area.text((x,y),line,fill=colour,font=font)
+      y += h
+
     return self
 
   def join(self,another,where="right",justify="center"):
     background = max(self.background,another.background)
-    if where == "up" or where == "down":
-      x = max(self.block.size[0], another.block.size[0])
-      y = self.block.size[1] + another.block.size[1]
-    else:
-      x = self.block.size[0] + another.block.size[0]
-      y = max(self.block.size[1], another.block.size[1])
+    xSelf     = self.block.size[0]
+    ySelf     = self.block.size[1]
+    xySelf    = (xSelf, ySelf)
+    xAnother  = another.block.size[0]
+    yAnother  = another.block.size[1]
+    xyAnother = (xAnother, yAnother)
 
-    new = Image.new("L",(x,y),background)
+    if where == "up" or where == "down":
+      xNew = max(xSelf,xAnother)
+      yNew = ySelf + yAnother
+    else:
+      xNew = xSelf + xAnother
+      yNew = max(ySelf,yAnother)
+
+    new = Image.new("L",(xNew,yNew),background)
     if   where == "up":
-      new.paste(self.block,
-                (self.__margin(another,'x',justify),another.block.size[1]))
-      new.paste(another.block,
-                (another.__margin(self,'x',justify),0                    ))
+      coordinatesSelf    = (self.__margin(xySelf,xyAnother,'x',justify),yAnother)
+      coordinatesAnother = (self.__margin(xyAnother,xySelf,'x',justify),0)
     elif where == "down":
-       new.paste(self.block,
-                 (self.__margin(another,'x',justify),0                 ))
-       new.paste(another.block,
-                 (another.__margin(self,'x',justify),self.block.size[1]))
+      coordinatesSelf    = (self.__margin(xySelf,xyAnother,'x',justify),0)
+      coordinatesAnother = (self.__margin(xyAnother,xySelf,'x',justify),ySelf)
     elif where == "left":
-      new.paste(self.block,
-                (another.block.size[0],self.__margin(another,'y',justify)))
-      new.paste(another.block,
-                (0,another.__margin(self,'y',justify)))
+      coordinatesSelf    = (xAnother,self.__margin(xySelf,xyAnother,'y',justify))
+      coordinatesAnother = (0,       self.__margin(xyAnother,xySelf,'y',justify))
     else:          # == "right"
-      new.paste(self.block,
-                (0,self.__margin(another,'y',justify)))
-      new.paste(another.block,
-                (self.block.size[0],another.__margin(self,'y',justify)))
+      coordinatesSelf    = (0,    self.__margin(xySelf,xyAnother,'y',justify))
+      coordinatesAnother = (xSelf,self.__margin(xyAnother,xySelf,'y',justify))
+
+    new.paste(self.block,   coordinatesSelf)
+    new.paste(another.block,coordinatesAnother)
 
     self.block = new
   
@@ -170,7 +149,7 @@ class Block():
 if __name__ == '__main__':
   img = Block((300,100),128)
   img2 = Block((200,200),255)
-  img2.text("Success")
-  img.text("9 C \n9 km/h",vertical="center")
+  img2.text("Success\nasdfadsf\nadsgfdgdfs\n9")
+  img.text("9 C \n9 km/h",horizontal="center")
   img.join(img2,"right")
   img.show()

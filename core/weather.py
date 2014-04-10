a3f5056d4ea7d37b79ea9e1041d9079acc28c6e6
paulfantom <paulfantom@gmail.com>
2014-04-10 21:26:08 +0200

    save weather to json

--- core/weather.py
+++ core/weather.py
@@ -15,12 +15,13 @@ class Weather:
 
     options="conditions/forecast"
     if tmpFile:
-      if type(tmpFile) == str:
-        self.tmpFile = tmpFile
-      else:
+      if type(tmpFile) != str:
         self.tmpFile = "/tmp/weatherStation.tmp"
+      else:
+        self.tmpFile = tmpFile
     else:
       self.tmpFile = False
+
     try:
       wundergroundResponse = urllib2.urlopen( "http://api.wunderground.com/api/"
                                              + apiKey
@@ -39,13 +40,16 @@ class Weather:
       self.data = None
       print "No weather data available"
 
+# Private
+
   def __save(self,data):
     try:
       with open(self.tmpFile,"r") as fileContent:
         cur = json.load(fileContent)
     except IOError:
+      print ( "No file specified in Weather object" )
       cur = None
-    
+
     try:
       if 'conditions' in data.keys():
         data['forecast'] = cur['forecast']
@@ -59,9 +63,12 @@ class Weather:
     with open(self.tmpFile,"w") as fileContent:
       json.dump(data,fileContent,indent=2,sort_keys=True)
 
+# Public
+
   def conditions(self,parameter=None,save=False):
     if self.data == None:
       return None
+
     if save:
       observation = self.data["current_observation"]
       formatedObservation = {
@@ -69,21 +76,21 @@ class Weather:
                            'humidity' :     observation["relative_humidity"],
                            'feeltemp' :     observation["feelslike_c"],
                            'pressure' :     observation["pressure_mb"],
-                           'presTrend': str(observation["pressure_trend"]),
+                           'presTrend':     observation["pressure_trend"],
                            'sky'      : str(observation["weather"]),
                            'sky_icon' : str(observation["icon_url"]),
-                           'wind_mph' : int(observation["wind_mph"]),
-                           'wind_kph' : int(observation["wind_kph"])
+                           'wind_mph' :     observation["wind_mph"],
+                           'wind_kph' :     observation["wind_kph"]
                           }
       self.__save({'conditions' : formatedObservation})
-    else:
-      try:
-        return self.data["current_observation"][parameter]
-      except KeyError:
-        print "Wrong paremeter: " + str(parameter)
-        return "---"
 
+    try:
+      return self.data["current_observation"][parameter]
+    except KeyError:
+      print "Wrong paremeter: " + str(parameter)
+      return "---"
 
+  
   def forecast(self,day,parameter=None,unit=None,save=False):
     if self.data == None:
       return None
@@ -100,8 +107,8 @@ class Weather:
                           'humidity'  :     forecast["avehumidity"],
                           'sky'       : str(forecast["conditions"]),
                           'sky_icon'  : str(forecast["icon_url"]),
-                          'wind_mph'  : int(forecast["avewind"]["mph"]),
-                          'wind_kph'  : int(forecast["avewind"]["kph"]),
+                          'wind_mph'  :     forecast["avewind"]["mph"]),
+                          'wind_kph'  :     forecast["avewind"]["kph"],
                           'weekday'   :     forecast["date"]["weekday_short"]
                          }
       string = 'forecast day ' + str(day)
@@ -125,11 +132,14 @@ class Weather:
       return forecast
 
 
-  def save(self,conditions=True,forecast=False):
-    if self.tmpFile:
+  def save(self,conditions=True,forecast=False,dataFile=None):
+    if dataFile != None:
+      self.tmpFile = dataFile
+
+    if conditions or forecast:
       if conditions:
-        self.conditions(True)
+        self.conditions(save=True)
       if forecast:
-        self.forecast(True)
+        self.forecast(save=True)
     else:
-      print ("No save file specified")
+      print ("No data to save")
